32aaacd4bedc209725a5805967bd77fe8df65b8f
Zac Rubin <zacrubin123@gmail.com>
2018-02-05 19:25:43 +0000

    Version 1.0.dev1

---
+++ parsertui.py
@@ -0,0 +1,171 @@
+from plateparser2 import PlateParser
+import os
+import platform
+
+
+__version__ = "1.0.dev1"
+
+
+class ParserTUI:
+    title = "Platereader Parser".upper()
+
+    def __init__(self):
+        self.parser = None
+        self.plate = None
+
+    def window(self):
+        self.clear()
+        print("=*={} - V{}".format(self.title, __version__), end="\n\n")
+        self.instructions()
+
+    def heading(self, header):
+        print("{}".format(header))
+
+    def clear(self):
+        plat = platform.system()
+        if plat == "Windows":
+            os.system("cls")
+        elif plat == "Linux":
+            os.system("clear")
+        else:
+            print("(Clear screen is not supported on this platform.)")
+
+    def prompt_yn(self, message):
+        yn = input(">> {} ['Y':YES, ENTER:NO]: ".format(message))
+        return yn == "Y" or yn == "y"
+
+    def mode_pick_input(self):
+        files = [x for x in os.listdir() if os.path.isfile(x)]
+        files = [x for x in files if os.path.splitext(x)[1] == ".DAT"]
+        print()
+        selected_files = self.select_from_list(files, n=1000, message="Choose DAT files", ask_all=True)
+        print()
+        self.parser = PlateParser(selected_files)
+
+    def mode_pick_dates(self):
+        selected_dates = self.select_from_list(self.parser.dates, n=2, message="Choose the range of dates (one at a time)")
+        print()
+        plate = self.parser.plate_from_daterange(selected_dates[0], selected_dates[1])
+        self.plate = plate
+
+    def write_file(self, path, data):
+        if os.path.exists(path):
+            print("File or directory with name {!r} already exists!".format(path))
+            overwrite = self.prompt_yn("Overwrite file?")
+            if not overwrite:
+                return False
+
+        elif not path:
+            print("File path was empty")
+            return False
+
+        with open(path, "w") as f:
+            f.write(data)
+            print("Written to {!r}".format(path))
+            return True
+
+    def mode_output_csv(self):
+        while True:
+            path = input(">> Name of output CSV file (eg 'data.csv'): ")
+            write = self.write_file(path, self.plate.to_csv())
+            if write:
+                break
+
+    def select_from_list(self, li, n=1, message=None, info=None, ask_all=False):
+        if info is None:
+            info = [""] * len(li)
+        print("==========")
+        print("OPT - ITEM")
+        print("----------")
+        for i, (x, inf) in enumerate(zip(li, info)):
+            out = "[{}] - {!r}".format(i, x)
+            if inf:
+                out += " ({})".format(inf)
+            print(out)
+        print()
+
+        if ask_all:
+            collect_all = self.prompt_yn("Select ALL available options?")
+        else:
+            collect_all = False
+
+
+        while True:
+            choices = []
+            if not collect_all:
+                while len(choices) < n:
+                    choice = self.prompt_number(message)
+                    if choice is None:
+                        break
+                    else:
+                        print("Selected option {}".format(choice))
+                        choices.append(choice)
+                    print()
+            else:
+                choices = [i for i in range(len(li))]
+            if not choices:
+                print("Error! Nothing selected!")
+                continue
+            else:
+                try:
+                    output = [li[i] for i in choices]
+                except IndexError as e:
+                    print("Error! Invalid choice(s)!")
+                    continue
+                else:
+                    break
+
+        self.window()
+        str_output = list(map(str, output))
+        print("Selected {!r}".format(str_output), end=" ")
+        happy = self.prompt_yn("Okay?")
+        if not happy:
+            output = self.select_from_list(li, n=n, message=message)
+            print()
+        return output
+
+    def prompt_number(self, message):
+        while True:
+            n_str = input(">> {} [option eg '0', press ENTER again to finish selection]: ".format(message))
+            if not n_str:
+                return None
+            try:
+                n = int(n_str)
+            except ValueError:
+                print("Error! Input was not a number!")
+                print()
+            else:
+                return n
+
+    def instructions(self):
+        print("Built by Zac Rubin.")
+        print()
+        print("Please ensure that the files have the .DAT extension.")
+        print("Please ensure that the files are in the same directory as this program.", end="\n\n")
+        print("Input options are shown in square brackets - eg ['Y'].")
+        print("When prompted for multiple options, press ENTER after each individual option.")
+        print("Remember that options are zero-indexed (ie 0 is for the first option).")
+        print()
+        print("***")
+        print()
+
+    def run(self):
+        self.window()
+        self.heading("SELECT INPUT FILES")
+        self.mode_pick_input()
+        print(end="\n\n")
+        self.window()
+        self.heading("SELECT DATA")
+        self.mode_pick_dates()
+        print(end="\n\n")
+        self.window()
+        print("Data loaded OKAY!", end="\n\n")
+        self.heading("WRITE DATA")
+        self.mode_output_csv()
+
+
+if __name__ == "__main__":
+    app = ParserTUI()
+    app.run()
+
+
---
+++ plateparser2.py
@@ -0,0 +1,160 @@
+import re
+import sys
+from datetime import timedelta, datetime, time
+import numpy as np
+import pandas as pd
+from string import ascii_uppercase
+from collections import OrderedDict
+
+
+pd.options.mode.chained_assignment = None
+
+
+class PlateParserError(Exception):
+    pass
+
+class PlateParser:
+    re_plate_well_data = "([A-H][0-1][0-9]):\s+([-+]?\d*\.\d+|\d+)"
+    re_plate_time = "Time:\s+([0-2][0-9]):([0-9][0-9]):([0-9][0-9])"
+    re_plate_date = "Date:\s+([0-3][0-9])/([0-1][0-9])/([0-9]+)"
+    re_plate_testname = "(^.*|\n)Testname:\s+(.+)"
+    
+    def __init__(self, paths):
+        self.last_timepoint = 0
+        self.time_elapsed = timedelta(seconds=0)
+        self._data = pd.DataFrame(columns=["testname"])
+        self.parse_files(paths)
+        self._data = self._data.sort_index()
+
+    def re_match_testname(self, string):
+        pat = re.compile(self.re_plate_testname, re.MULTILINE)
+        mat = pat.match(string)
+
+        if mat is not None:
+            hit  = mat.groups()[1]
+            return hit
+
+    def re_match_time(self, string):
+        pat = re.compile(self.re_plate_time, re.MULTILINE)
+        mat = pat.search(string)
+
+        if mat is not None:
+            h = int(mat.groups()[0])
+            m = int(mat.groups()[1])
+            s = int(mat.groups()[2])
+            return (h, m, s)
+
+    def re_match_date(self, string):
+        pat = re.compile(self.re_plate_date, re.MULTILINE)
+        mat = pat.search(string)
+        if mat is not None:
+            day = int(mat.groups()[0])
+            month = int(mat.groups()[1])
+            year = int(mat.groups()[2])
+            return(day, month, year)
+
+    def re_match_well(self, string):
+        pat = re.compile(self.re_plate_well_data)
+        mat = pat.findall(string)
+        if mat is not None:
+            return mat
+
+    def calculate_time_delta(self):
+        current_as_delta = timedelta(hours=self.current_timepoint.hour, minutes=self.current_timepoint.minute, seconds=self.current_timepoint.second)
+        last_as_delta = timedelta(hours=self.last_timepoint.hour, minutes=self.last_timepoint.minute, seconds=self.last_timepoint.second)
+        if current_as_delta < last_as_delta:
+            current_as_delta += timedelta(days=1)
+        return current_as_delta - last_as_delta
+
+    def parse_files(self, paths):
+        if paths is None or not paths:
+            raise PlateParserError("No input paths given!")
+        for file_idx, path in enumerate(paths):
+            with open(path) as f:
+                data = f.read()
+                datapoints = data.split("-"*86)
+                for datapoint_idx, datapoint in enumerate(datapoints):
+                    testname = self.re_match_testname(datapoint)
+                    time = self.re_match_time(datapoint)
+                    date = self.re_match_date(datapoint)
+                    dt = datetime(day=date[0], month=date[1], year=date[2], hour=time[0], minute=time[1], second=time[2])
+                    data = self.re_match_well(datapoint)
+
+                    for pair in data:
+                        well, value = pair[0], pair[1]
+                        self._data.loc[dt, well] = value
+                    self._data.loc[dt, "testname"] = testname
+
+    @property
+    def testnames(self):
+        return list(set(self._data["testname"]))
+    @property
+    def dates(self):
+        return self._data.index
+
+    def plate_from_testname(self, testname):
+        if testname not in self.testnames:
+            raise ValueError("Testname {!r} is not a valid testname for your data".format(testname))
+        data = self._data[self._data["testname"] == testname]
+        return Plate(data, testname)
+
+    def plate_from_daterange(self, start, end):
+        if not isinstance(start, datetime):
+            raise TypeError("Start date must be datetime")
+        if not isinstance(end, datetime):
+            raise TypeError("Start date must be datetime")
+        if not start in self.dates:
+            raise ValueError("Invalid start date! Must be exact datetime match")
+        if not end in self.dates:
+            raise ValueError("Invalid end date! Must be exact datetime match")
+
+        data = self._data[(self._data.index >= start) & (self._data.index <= end)]
+        n_testnames = len(set(data['testname']))
+        if n_testnames > 1:
+            raise ValueError("More than one testname in the data range!")
+        return Plate(data, data['testname'].values[1])
+        
+
+class Plate:
+    def __init__(self, well_data, testname):
+        self.data = well_data
+        self.testname = testname
+
+        elapsed = well_data.index - well_data.index[0]
+        datetime = well_data.index.values
+
+        self.data['elapsed'] = elapsed
+        self.data['datetime'] = datetime
+        self.data.index = pd.Series([i for i, x in enumerate(self.data.index)])
+        cols = self.data.columns.tolist()
+        move = [ "testname", "elapsed", "datetime"]
+        for x in move:
+            cols.remove(x)
+        for i, x in enumerate(move):
+            cols.insert(i, x)
+        self.data = self.data[cols]
+
+    def __repr__(self):
+        return("Plate({!r})".format(self.testname))
+    def __str__(self):
+        return(str(self.data))
+
+    @property
+    def date_range(self):
+        return "{} - {}".format(self.data["datetime"].iloc[0], self.data["datetime"].iloc[-1])
+
+    @property
+    def time_range(self):
+        return "{} - {}".format(self.data['elapsed'].iloc[0], self.data['elapsed'].iloc[-1])
+
+    @property
+    def to_csv(self):
+        return self.data.to_csv
+
+if __name__ == "__main__":
+    paths = sys.argv[1:]
+    parser = PlateParser(paths)
+    start = parser.dates[1]
+    end = parser.dates[-1]
+    plate = parser.plate_from_daterange(start, end)
+    print(repr(plate.to_csv()))
